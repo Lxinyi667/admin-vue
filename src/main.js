334f16ed81159bfd41d813a622bffb21785ba8de
Lxinyi667 <1716186890@qq.com>
2023-04-24 16:46:08 +0800

    feat(custom): 配置路由和404

--- src/main.js
+++ src/main.js
@@ -4,11 +4,21 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+// 引入图标
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 const app = createApp(App)
+// 引入图标
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+  }
 
 app.use(createPinia())
 app.use(router)
+app.use(ElementPlus)
+
+import 'virtual:windi.css'
 
 app.mount('#app')
---
+++ src/views/login/index.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+ddd
+    </div>
+</template>
+
+<style scoped>
+
+</style>
