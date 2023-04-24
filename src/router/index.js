b84baa9533b0456c91d3c27a1079eff902641356
Lxinyi667 <1716186890@qq.com>
2023-04-24 21:07:06 +0800

    feat(custom): 全局路由拦截实现登录情况判断

--- src/router/index.js
+++ src/router/index.js
@@ -26,5 +26,28 @@ const router = createRouter({
   history: createWebHistory(),
   routes
 })
+// 添加全局路由拦截，对登录情况进行完善
+router.beforeEach(async (to, from, next) => {
+  const token = getToken()
+  // 没有登录，强制跳转回登录页
+  if (!token && to.path != '/login') {
+    toast('请先登录', 'error')
+    return next({ path: '/login' })
+  }
+
+  // 防止重复登录
+  if (token && to.path == '/login') {
+    toast('请勿重复登录', 'error')
+    return next({ path: from.path ? from.path : '/' })
+  }
+
+  const { getStoreInfo } = useAdminStore()
+  //  如果登录了，就获取用户信息，存储到 pinia
+  if (token) {
+    await getStoreInfo()
+  }
+
+  next()
+})
 
 export default router
